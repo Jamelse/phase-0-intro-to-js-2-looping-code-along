fe325d18ce2505b780598d621dff1ec8d1addd58
Jacob <jacobamelse@hotmail.com>
2022-07-23 01:40:15 -0500

    commit tree 70c4876c3f22663ec7181c1778c298c4cd3db23b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts)
+
+
+
+function writeCards(names, adjective){
+    const message = [];
+    for (let i=0; i < names.length; i++){
+    message.push(`Thank you, ${names[i]}, for the wonderful ${adjective} gift!`);}
+return message;
+}
+
+function countDown(){
+    let i = 10
+    while (i > 0){
+        console.log( i-= 1);
+    }
+    console.log(i);
+}
